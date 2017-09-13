51880c50e5dc2e08ac863e54155f0dbf4afc2b34
artem.radchenko <artem.radchenko@globallogic.com>
2017-09-13 17:59:19 +0300

    ndvi python finished

--- ndvi.sh
+++ ndvi.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-if [ -z $1 ]; then
-	echo "no parameter"
-fi
-rm -rf /home/artem/captures/in/*
-cp $1 /home/artem/captures/in/
 python processNGB.py /home/artem/captures/in/ /home/artem/captures/out/ -0.05 0.7 1
-rsync
