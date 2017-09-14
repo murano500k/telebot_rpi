e4d070b28a5a5f814758ca517089651f7c60d147
artem.radchenko <artem.radchenko@globallogic.com>
2017-09-14 12:41:02 +0300

    history home 5

--- history_home.sh
+++ history_home.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 IP_PI=192.168.0.139
 ssh pi@$IP_PI /home/pi/telebot_rpi/history.sh
-~/projects/telebot_rpi/syncpi.sh
+~/projects/telebot_rpi/syncpi.sh captures
 ~/projects/telebot_rpi/copy_image.sh
 
 
