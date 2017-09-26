3150ea1fa962d22b842cc7cedaf3a14c2a4e2705
artem.radchenko <artem.radchenko@globallogic.com>
2017-09-26 12:41:27 +0300

    moved env to env_vars

--- ndvi.sh
+++ ndvi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python /home/artem/projects/telebot_rpi/processNGB.py /home/artem/captures/in/ /home/artem/captures/out/ -0.35 1 0
+python /home/artem/projects/telebot_rpi/processNGB.py /home/artem/captures/in/ /home/artem/captures/out/ -0.35 1.5 0
